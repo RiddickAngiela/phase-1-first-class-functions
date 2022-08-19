b53fecc1eca50a722dbfe77553d295c08fb09777
Riddick <riddick.angiela@student.moringaschool.com>
2022-08-19 16:09:07 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const spy = chai.spy()
+function receivesAFunction(spy){
+    spy()
+}
+
+function returnsANamedFunction(){
+    const drew = (a, b) => a + b;
+    return drew;
+}
+
+function returnsAnAnonymousFunction(){
+    return () => "I";
+}
